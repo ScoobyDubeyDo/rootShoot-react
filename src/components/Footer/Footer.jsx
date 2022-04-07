14c38fcd1dbda84af5689e56155f60a3140eee0d
aman-can <aman.dubey1998@gmail.com>
2022-04-07 19:49:27 +0530

    refactor: Fixed the footer layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,47 +7,68 @@ import { FiLinkedin } from "react-icons/fi";
 
 export const Footer = () => {
     return (
-        <footer>
-            <span className="statement heading-6 rootShoot-margin-center">
-                Made with
-                <img src={imgLogo} className="responsive-image" alt="logo" />
-                by Aman
-            </span>
-            <div className="media-links">
-                <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://www.instagram.com/scoobydubeydo/"
-                    className="icon-btn-green"
-                >
-                    <AiOutlineInstagram />
-                </a>
-                <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://github.com/aman-can/"
-                    className="icon-btn-green"
-                >
-                    <VscGithub />
-                </a>
-                <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://twitter.com/ScoobyDubeyDo"
-                    className="icon-btn-green"
-                >
-                    <RiTwitterLine />
-                </a>
-                <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://www.linkedin.com/in/dubey-aman/"
-                    className="icon-btn-green"
-                >
-                    <FiLinkedin />
-                </a>
+        <footer className="vertical-list">
+            <div className="vertical-list sub-footer">
+                <span>
+                    <img
+                        src={imgLogo}
+                        className="responsive-image"
+                        alt="logo"
+                    />
+                </span>
+                <span className="horizontal-list footer-text">
+                    <span className="vertical-list">
+                        <p className="heading-6">Quick Links</p>
+                        <p className="text-subtitle">Contact Us</p>
+                        <p className="text-subtitle">FAQs</p>
+                        <p className="text-subtitle">Privacy policy</p>
+                    </span>
+                    <span className="vertical-list">
+                        <p className="heading-6">Catalog</p>
+                        <p className="text-subtitle">Cacti</p>
+                        <p className="text-subtitle">Succulent</p>
+                        <p className="text-subtitle">Monstera</p>
+                    </span>
+                </span>
+                <div className="vertical-list">
+                    <span className="text-align-center">Social Links</span>
+                    <span className="horizontal-list media-links">
+                        <a
+                            target="_blank"
+                            rel="noreferrer"
+                            href="https://www.instagram.com/scoobydubeydo/"
+                            className="icon-btn-green"
+                        >
+                            <AiOutlineInstagram />
+                        </a>
+                        <a
+                            target="_blank"
+                            rel="noreferrer"
+                            href="https://github.com/aman-can/"
+                            className="icon-btn-green"
+                        >
+                            <VscGithub />
+                        </a>
+                        <a
+                            target="_blank"
+                            rel="noreferrer"
+                            href="https://twitter.com/ScoobyDubeyDo"
+                            className="icon-btn-green"
+                        >
+                            <RiTwitterLine />
+                        </a>
+                        <a
+                            target="_blank"
+                            rel="noreferrer"
+                            href="https://www.linkedin.com/in/dubey-aman/"
+                            className="icon-btn-green"
+                        >
+                            <FiLinkedin />
+                        </a>
+                    </span>
+                </div>
             </div>
-            <span className="copyright rootShoot-margin-center text-subtitle">
+            <span className="copyright rootShoot-margin-center text-align-center text-subtitle">
                 copyright &copy; 2021 all rights reserved by rootShoot.
             </span>
         </footer>
